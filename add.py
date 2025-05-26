4ecf6dee9cb721ae2ef2e9afce65213c69548c0c
tbongiorno <163329687+tbongiorno@users.noreply.github.com>
2025-05-26 09:13:11 -0400

    5/26/25 A small touch-up

--- add.py
+++ add.py
@@ -3,7 +3,41 @@ import json
 
 class Add:
     def add_game(ng, i):
-        #Search RAWG API for game from title (ng parameter)
-        #Add the game with all parameters to 
+        #Search RAWG API for game data from title (ng parameter)
+        #Search isthereanydeal API for price details
+        #Add the game with all necessary parameters to data
 
+        #Get details of the game
+        # ENDPOINT_RAWG = f"https://api.rawg.io/api/games/{i}"
+        # API_KEY_RAWG = "" #NEED TO SET UP
+        # CONFIG_RAWG = {
+        #     "id": f"{i}"
+        # }
+        # response_rawg = requests.get(url=ENDPOINT_RAWG, json= ,auth=)
+        # data = response_rawg.json()
+
+
+        #Prices
+        # ENDPOINT_DEAL = "https://api.isthereanydeal.com/games/prices/v3"
+        # API_KEY_DEAL = "" #NEED TO SET UP
+        # REQUEST_BODY_SCHEMA = [f"{i}"]
+        # CONFIG_DEAL = {
+        #       "country": "US",
+        #       "deals": False,
+        #       "vouchers": False,
+        #       "capacity": 3,
+        #       "shops": "steam,gog,epic",
+        # }
+        # response_deal = requests.post(url=ENDPOINT_DEAL, json= ,auth= )
+
+        # game = {
+        #     "name": ng,
+        #     "image": data["background_image"],
+        #     "price": "",
+        #     "on_sale": "",
+        #     "reviews": "",
+        #     "interest": "",
+        # }
+
+        # return game
         return 0
--- sort.py
+++ sort.py
@@ -1,4 +1,34 @@
 class Sort:
     def sort_games(games):
         #SORT BY LARGEST SCORE AT TOP, IF TIE THEN BY INTEREST, IF TIE THEN BY PRICE, IF TIE, THEN BY NAME
+        for i in range(len(games) - 1, 0, -1):
+            switch = False
+            for j in range(i):
+                if compare_games(games[j], games[j+1]):
+                    games[j], games[j + 1] = games[j + 1], games[j]
+                    switch = True
+            if not switch:
+                break
+
         return games        
+
+def compare_games(game_1, game_2):
+    if game_1["score"] > game_2["score"]:
+        return True
+    elif game_1["score"] < game_2["score"]:
+        return False
+    
+    if game_1["interest"] > game_2["interest"]:
+        return True
+    elif game_1["interest"] < game_2["interest"]:
+        return False
+    
+    if game_1["price"] > game_2["price"]:
+        return True
+    elif game_1["price"] < game_2["price"]:
+        return False
+    
+    if game_1["title"][0] < game_2["title"][0]:
+        return True
+    else:
+        return False
